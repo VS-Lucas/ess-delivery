170c47116aa33e0d72e21b45d3232949bfe25b15
Lucas <lucasvanlume@hotmail.com>
2023-03-02 16:16:06 -0300

    Adding background and form container in component 'RegisterView.vue'

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,7 +1,25 @@
 <template>
-    <div>
-        Olá
+  <div>
+    <div class="bg-gradient-to-b from-primary-red-bg to-primary-red-register">
+      <div class="bg-no-repeat bg-fixed md:grid md:grid-cols-7 md:gap-5"
+           style="background-image: url('img/food_register_bg.png');
+                  background-position: right">
+        <div class="md:col-start-2 md:col-span-4 mt-5 md:mt-0">
+            <br>
+          <form action="#" method="POST">
+            <div class="flex justify-center items-center">
+              <div class="shadow sm:overflow-hidden sm:rounded-md w-full">
+                <div class="space-y-6 bg-gray-50 px-4 py-5 sm:p-6">
+                  <br>
+                    OLÁ
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 
