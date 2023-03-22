804df2cc8a6df708b8704d8ad457f6e294795946
Lucas <lucasvanlume@hotmail.com>
2023-03-22 17:57:36 -0300

    [PE-52] Setting up 'firebase-admin' on the server

--- server/index.js
+++ server/index.js
@@ -1,11 +1,27 @@
-const express =require('express')
+import express from 'express';
+import admin from 'firebase-admin';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+
 const app = express();
-const port = 8000
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//Configuração firebase
+admin.initializeApp({
+  credential: admin.credential.cert("../firebaseConfig.json")
+});
+
 
 app.get('/', (req, res)=>{
     res.send('HELLO WORLD')
 })
 
-app.listen(port, ()=>{
-    console.log('Servidor ON')
-})
+
+app.listen(3000, ()=>{
+    console.log('Servidor ON em http://localhost:3000')
+  });
