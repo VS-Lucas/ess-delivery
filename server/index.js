174e480f4700af36fd96907338edc3f0df4677ea
Lucas <lucasvanlume@hotmail.com>
2023-03-22 18:29:00 -0300

    [PE-52] Addition of the POST route on the server for '/register-restaurant' requests

--- server/index.js
+++ server/index.js
@@ -16,12 +16,26 @@ admin.initializeApp({
   credential: admin.credential.cert("../firebaseConfig.json")
 });
 
+//Rota Post do cadastro de restaurante
+app.post('/register-restaurant', (req, res) => {
+  console.log('POST restaurant');
+  const data = req.body;
+  admin.firestore()
+    .collection('restaurantes')
+    .add(data)
+    .then(docRef => {
+      const responseData = { id: docRef.id, ...data };
+      res.json(responseData);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).send('Erro ao salvar restaurante');
+    });
+});
 
-app.get('/', (req, res)=>{
-    res.send('HELLO WORLD')
-})
 
 
 app.listen(3000, ()=>{
-    console.log('Servidor ON em http://localhost:3000')
-  });
+  console.log('Servidor ON em http://localhost:3000')
+});
+
