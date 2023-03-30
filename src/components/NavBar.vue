ece4c51d9194cd4e838cc43220cbabaf86cee6e5
Bonna Borsoi <bonnagilborsoi@gmail.com>
2023-03-30 10:45:49 -0300

    [PE-49] view of Client Home and images

---
+++ src/components/NavBar.vue
@@ -0,0 +1,32 @@
+<template>
+
+    <nav class="bg-[#A62C21] px-2 sm:px-6 py-3.5 dark:bg-gray-900 fixed w-full z-20 top-0 left-0 ">
+      <div class="container flex flex-wrap items-center justify-between mx-auto">
+    
+        <a class="flex items-center">
+            <img src="@\assets\img\logo_navbar.png" class="h-9 mr-3 sm:h-16" alt="Logo"> <!-- sm:h-16 altera o tamanho da logo e consequentemente o da nav bar-->
+        </a>
+        
+        <div flex md:order-2>
+            <button class="px-5 mr-3 md:ml-15 ">
+            <img src="@\assets\img\user_icon.png" alt="Descrição da imagem" class=" h-10 w-11" >
+            </button>
+    
+        </div>
+    
+      </div>
+    
+    </nav>
+    
+
+    </template>
+        
+        
+ <script scoped>
+    export default {
+        name: 'NavBar',
+        props: {
+        msg: String
+        }
+    }
+</script>
