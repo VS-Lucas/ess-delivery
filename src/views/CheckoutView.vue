5c2bfc4dc698f32b2baa05a033ae496325ae0c1e
VS-Lucas <ufpelucasv@gmail.com>
2023-04-03 22:10:49 -0300

    [PE-56] fixing add checkout page structure commmit

---
+++ src/views/CheckoutView.vue
@@ -0,0 +1,40 @@
+<template>
+    <body class="bg-[#261918] min-h-screen">
+        <div class="w-full">
+            <nav class="bg-[#541F1B] border-gray-200 flex items-center p-4">
+                <button type="button">
+                    <img src="../assets/img/back-button.png" alt="Back button">
+                </button>
+                <span class="text-3xl text-white text-center flex-1 mr-[60px]">Checkout</span>
+            </nav>
+        </div> 
+        <div class="mt-10">
+            <div class="grid grid-cols-7">
+                <div class="col-start-2 col-span-2 bg-[#A62C21] h-[280px] rounded-[10px]">
+                    <div class="mt-[20px] ml-[20px]">
+                        <h1 class="text-3xl text-white">Meu carrinho</h1>
+                    </div>
+                </div>
+                <div class="col-start-5 col-span-2 bg-[#A62C21] h-[280px] rounded-[10px]">
+                    <div class="mt-[20px] ml-[20px]">
+                        <h1 class="text-3xl text-white">Pagamento</h1>
+                    </div>
+                </div>
+                <div class="col-start-2 col-span-5 bg-[#A62C21] h-[200px] rounded-[10px] mt-10">
+                    <div class="mt-[20px] ml-[20px]">
+                        <h1 class="text-3xl text-white">Endereço de entrega</h1>
+                    </div>
+                </div>
+                <div class="col-start-4 col-span-1 bg-[#A62C21] h-[50px] rounded-[10px] mt-10">
+                    <div class="text-center mt-[5px]">
+                        <h1 class="text-3xl text-white">Confirmar</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+  </template>
+
+<script>
+
+</script>
