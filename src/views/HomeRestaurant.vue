554715374813b1d459a40973a31e58128c0a12a5
acarlaguerra <acarlaguerra@gmail.com>
2023-04-05 22:25:00 -0300

    [PE-50]-Added Dashboard Table and updated Home View

--- src/views/HomeRestaurant.vue
+++ src/views/HomeRestaurant.vue
@@ -146,6 +146,159 @@
 
             </div>
 
+            <div class="h-76 mb-4 rounded bg-[#DDDDDD] dark:bg-gray-800">
+
+               <h2 class="flex items-start justify-start pt-4 pl-4" style="font-size: 20px; font-weight: 800;">Lista de Pedidos</h2>
+
+               <div class="grid pt-4 pb-4">
+                     
+                     <div class="relative overflow-x-auto">
+                        <table class="w-full text-sm text-center font-semibold text-gray-500 dark:text-gray-600">
+                           <thead class="text-xs text-gray-800 uppercase bg-[#DDDDDD] dark:bg-gray-800 dark:text-gray-700">
+                                 <tr>
+                                    <th scope="col" class="px-6 py-3 text-left">
+                                       No
+                                    </th>
+                                    <th scope="col" class="px-12 py-3">
+                                       ID
+                                    </th>
+                                    <th scope="col" class="px-12 py-3">
+                                       STATUS
+                                    </th>
+                                    <th scope="col" class="px-12 py-3">
+                                       LOCAL DA ENTREGA
+                                    </th>
+                                    <th scope="col" class="px-12 py-3">
+                                       PAGAMENTO
+                                    </th>
+                                    <th scope="col" class="px-12 py-3">
+                                       VALOR
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                       <span class="sr-only">Mais info</span>
+                                    </th>
+                                 </tr>
+                           </thead>
+                           <tbody>
+                                 <tr class="bg-[#DDDDDD] border-b font-medium dark:bg-gray-800 dark:border-gray-700 hover:bg-white dark:hover:bg-gray-600">
+                                    <th scope="row" class="px-6 py-4 text-left font-medium text-gray-900 whitespace-normal dark:text-gray-900">
+                                       01
+                                    </th>
+                                    <td class="px-12 py-4">
+                                       #12451
+                                    </td>
+                                    <td class="px-12 py-4r">
+                                       <span class="bg-[#F26938] bg-opacity-50  text-gray-600 text-sm font-medium mr-2 px-5 py-1.5 rounded-full dark:bg-gray-700 dark:text-gray-600">
+                                          Em preparo
+                                       </span>
+                                    </td>
+                                    <td class="px-12 py-4">
+                                       Rua da Harmonia
+                                    </td>
+                                    <td class="px-12 py-4">
+                                       Dinheiro-Entrega
+                                    </td>
+                                    <td class="px-12 py-4">
+                                       39,90
+                                    </td>
+                                    <td class="px-6 py-4 text-right">
+                                       <!-- <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Mais info...</a> -->
+                                       
+                                       <!-- Modal toggle -->
+                                       <button @click="showDetail" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
+                                       Mais info...
+                                       </button>
+
+                                       <!-- Main modal -->
+                                       <div v-if="this.modalCard">
+                                          <div class="fixed top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] md:h-full">
+                                             <div class="relative w-full h-full max-w-2xl md:h-auto">
+                                                <!-- Modal content -->
+                                                <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                                                      <!-- Modal header -->
+                                                      <div class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
+                                                         <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                                                            Detalhes do Pedido
+                                                         </h3>
+                                                         <button @click="showDetail" type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="orderListModal">
+                                                            <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                                                            <span class="sr-only">Close modal</span>
+                                                         </button>
+                                                      </div>
+                                                      <!-- Modal body -->
+                                                      <div class="p-6 space-y-6">
+                                                         <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                                                            With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply.
+                                                         </p>
+                                                         <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                                                            The European Union's General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant to ensure a common set of data rights in the European Union. It requires organizations to notify users as soon as possible of high-risk data breaches that could personally affect them.
+                                                         </p>
+                                                      </div>
+                                                </div>
+                                             </div>
+                                          </div>
+                                       </div>
+
+                                    </td>
+                                 </tr>
+                                 <tr class="bg-[#DDDDDD] border-b font-medium dark:bg-gray-800 dark:border-gray-700 hover:bg-white dark:hover:bg-gray-600">
+                                    <th scope="row" class="px-6 py-4 text-left font-medium text-gray-900 whitespace-normal dark:text-gray-900">
+                                       02
+                                    </th>
+                                    <td class="px-12 py-4">
+                                       #12452
+                                    </td>
+                                    <td class="px-12 py-4">
+                                       <span class="bg-[#F26938] bg-opacity-50  text-gray-600 text-sm font-medium mr-2 px-5 py-1.5 rounded-full dark:bg-gray-700 dark:text-gray-600">
+                                          Em preparo
+                                       </span>
+                                    </td>
+                                    <td class="px-12 py-4">
+                                       Retirar na loja
+                                    </td>
+                                    <td class="px-12 py-4">
+                                       Débito-Site
+                                    </td>
+                                    <td class="px-12 py-4">
+                                       51,00
+                                    </td>
+                                    <td class="px-6 py-4 text-right">
+                                       <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Mais info...</a>
+                                    </td>
+                                 </tr>
+                                 <tr class="bg-[#DDDDDD] dark:bg-gray-800 font-medium hover:bg-white dark:hover:bg-gray-600">
+                                    <th scope="row" class="px-6 py-4 text-left font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                       03
+                                    </th>
+                                    <td class="px-12 py-4">
+                                    #12453
+                                    </td>
+                                    <td class="px-12 py-4">
+                                       <span class="bg-[#F26938] bg-opacity-50  text-gray-600 text-sm font-medium mr-2 px-5 py-1.5 rounded-full dark:bg-gray-700 dark:text-gray-600">
+                                          Em preparo
+                                       </span>
+                                    </td>
+                                    <td class="px-12 py-4">
+                                       Fenda do Biquíni
+                                    </td>
+                                    <td class="px-12 py-4">
+                                       PIX-Site
+                                    </td>
+                                    <td class="px-12 py-4">
+                                       19,90
+                                    </td>
+                                    <td class="px-6 py-4 text-right">
+                                       <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Mais info...</a>
+                                    </td>
+                                 </tr>
+                           </tbody>
+                        </table>
+                     </div>
+
+               </div>
+
+            </div>
+
             <div class="grid grid-cols-4 gap-4 pb-2 text-white" style="font-size: 18px; font-weight: 700;">
                
                <h2 class="flex items-start justify-start col-start-0 col-span-2">Média de pedidos</h2>
@@ -154,10 +307,10 @@
 
             </div>
 
-            <div class="grid grid-cols-4 gap-4 h-36">
+            <div class="grid grid-cols-4 gap-4 col-start-1 col-span-2 h-36">
 
-               <div class="flex items-center justify-center rounded bg-gray-50 h-32 dark:bg-gray-800 col-start-0 col-span-2">
-                  <p class="text-2xl text-gray-400 dark:text-gray-500">+</p>
+               <div class="flex items-center justify-center rounded bg-gray-50 dark:bg-gray-800">
+                  <img src="..\assets\img\graphic.png" alt="graphichome">
                </div>
 
                <div class="flex items-center justify-center rounded bg-gray-50 h-32 dark:bg-gray-800">
@@ -174,3 +327,30 @@
 
    </div>
 </template>
+
+<script>
+// import axios from 'axios';
+
+export default ({
+   name: 'HomeRestaurant',
+   data() {
+      return {
+         orders: {
+            order_id: '12451',
+            items: ['parmegiana', 'coca', 'batata'],
+            delivery_place: 'Rua da Harmonia',
+            price: '93,00',
+            form_of_payment: 'pix'
+         },
+         modalCard: false
+      }
+   },
+   methods: {
+      showDetail() {
+         console.log(this.modalCard)
+         this.modalCard = !this.modalCard
+         console.log(this.modalCard)
+      }
+   }
+})
+</script>
