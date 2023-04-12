816cf40bb390e4298148d82d835a7e5596fd0987
Bonna Borsoi <bonnagilborsoi@gmail.com>
2023-04-11 22:11:46 -0300

    [PE-49] cleaning code and indentation

--- src/views/ClientHome.vue
+++ src/views/ClientHome.vue
@@ -1,30 +1,30 @@
 <template>
-    <div class="clienthome bg-[#261918] min-h-screen relative">
-       <NavBar /> <!-- NavBar dos componentes-->
-        <div class="absolute left-40 top-32"> <!-- Button "Meus Pedidos"-->
-          <button type="button" @click='goToOrderHistory()' class="focus:outline-none text-white bg-[#541F1B] hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-l px-7 py-3 mr-10 mb-2">Meus pedidos</button>
-        </div>
-  
-        <div class= "absolute right-72 top-32"> <!-- Search Bar, onde muda o tamanho é em pl-10 na linha 15, e na linha 8 define a posição, relativa a div da linha 2-->
-          <form>   
-            <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only ">Search</label>
-            <div class="relative">
-                <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                    <svg aria-hidden="true" class="w-5 h-5 text-gray-500 " fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
-                </div>
-                <input type="search" id="default-search" class="block w-100 p-10 pl-10 text-sm py-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 " placeholder="Search..." required>
-                <button type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 ">Search</button>
+  <div class="clienthome bg-[#261918] min-h-screen relative">
+    <NavBar /> <!-- NavBar dos componentes-->
+    <div class="absolute left-40 top-32"> <!-- Button "Meus Pedidos"-->
+      <button type="button" @click='goToOrderHistory()' class="focus:outline-none text-white bg-[#541F1B] hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-l px-7 py-3 mr-10 mb-2">Meus pedidos</button>
+    </div>
+
+    <div class= "absolute right-72 top-32"> <!-- Search Bar, onde muda o tamanho é em pl-10 na linha 15, e na linha 8 define a posição, relativa a div da linha 2-->
+      <form>   
+        <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only ">Search</label>
+        <div class="relative">
+            <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                <svg aria-hidden="true" class="w-5 h-5 text-gray-500 " fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
             </div>
-        </form>
+            <input type="search" id="default-search" class="block w-100 p-10 pl-10 text-sm py-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 " placeholder="Search..." required>
+            <button type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 ">Search</button>
         </div>
-  
-     <!-- Botão "Carrinho"-->
+    </form>
+    </div>
+
+      <!-- Botão "Carrinho"-->
     <button  @click='goToShoppingCart()' type="submit" class=" hover:bg-red-800 focus:ring-4 absolute right-32 top-32 focus:outline-none bg-[#A62C21]  focus:ring-red-300 font-medium rounded-lg text-l inline-block px-6 py-1 mr-10 mb-2 ">
         <img src="@\assets\img\carrinho.png" alt="Carrinho de Compras" class=" h-10 w-11" >
     </button>
 
-
-    <div  class=" mx-auto max-w-6xl justify-center px-6 md:flex md:space-x-7 xl:px-0 pt-52 " > <!-- Cards dos restaurantes e das comidas-->
+    <!-- Cards dos restaurantes e das comidas-->
+    <div class=" mx-auto max-w-6xl justify-center px-6 md:flex md:space-x-7 xl:px-0 pt-52" >
       <!-- Botão dos restaurantes -->
         <div @click="goToRestaurants()">
           <button class="focus:outline-none focus:ring-4 focus:ring-red-300">  
@@ -40,89 +40,74 @@
         </div>
     </div>
 
-    <!-- <div class=" absolute flex justify-center space-x-10 bottom-12 bg-cover inset-x-0" style="font-family: Montserrat">-->
-      <!-- Flexbox do cardápio-->
-        <!-- <div class=" absolute flex   items-center justify-center  bottom-10 bg-cover inset-x-0 " >
-          <div v-for="prato in pratos" :key="prato.id">
-            <button class="focus:outline-none text-white bg-[#541F1B] hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-l px-7 py-3 mr-10 mb-2 max-w-xs transform hover:scale-105 transition duration-300"
-            @click="addToCart(prato)">
-              <h2 style="font-family: 'Montserrat', sans-serif;  font-weight: 600; font-size: 1.2rem; line-height: 1.5rem; overflow: hidden; text-overflow: ellipsis; white-space: nowrap;">{{ prato.nome }}</h2>
-              <img :src="prato.url" :alt="prato.nome" class= ' object-scale-down h-28 w-full mx-auto block'>
-              <h2 style="font-size: 0.9rem; line-height: 1.2rem; overflow: hidden; text-overflow: ellipsis; white-space: nowrap;">{{ prato.descricao }}</h2>
-              <h2 style="font-size: 1rem; line-height: 1.2rem;">{{ prato.preco }}</h2>
-            </button>
-          </div>
-        </div> -->
-
-      <div class="text-xl font-bold text-white px-28 py-3"> Bode do Nô </div>
-      <div class=" flex flex-wrap items-center space-x-12 justify-center py-4 bg-cover inset-x-0  " > <!-- Flexbox do cardápio de pratos-->
-        <div v-for="prato in pratos_1" :key="prato.id">
-          <button class=" w-56 h-56 bg-[#541F1B] border-gray-200 rounded-lg shadow transform hover:scale-105 transition duration-300 " @click="addToCart(prato)">
-            <a href="#">
-              <img :src="prato.url" :alt="prato.nome" class= ' object-scale-down h-28 w-full  mx-auto block'>
-            </a>
-            <div class="px-5 pb-5">
-                <a href="#">
-                    <h5 class="text-lg font-semibold tracking-tight text-white ">{{prato.nome}}</h5>
-                </a>
-                <div class="text-xs text-white">{{prato.descricao}} </div>
-                <div class="flex items-center justify-between p-1">
-                    <div class="text-xl font-bold text-white">{{prato.preco}} </div>
-                    <a href="#" class="text-white bg-blue-700   font-medium rounded-lg text-sm px-1 py-1.5 text-center ">Add to cart</a>
-                </div>
+    <!-- Segundo restaurante -->
+    <div class="text-xl font-bold text-white px-28 py-3"> Bode do Nô </div>
+    <div class=" flex flex-wrap items-center space-x-12 justify-center py-4 bg-cover inset-x-0  " > <!-- Flexbox do cardápio de pratos-->
+      <div v-for="prato in pratos_1" :key="prato.id">
+        <button class=" w-56 h-56 bg-[#541F1B] border-gray-200 rounded-lg shadow transform hover:scale-105 transition duration-300 " @click="addToCart(prato)">
+          <a href="#">
+            <img :src="prato.url" :alt="prato.nome" class= 'object-scale-down h-28 w-full mx-auto block'>
+          </a>
+          <div class="px-5 pb-5">
+              <a href="#">
+                  <h5 class="text-lg font-semibold tracking-tight text-white ">{{prato.nome}}</h5>
+              </a>
+              <div class="text-xs text-white">{{prato.descricao}} </div>
+              <div class="flex items-center justify-between p-1">
+                  <div class="text-xl font-bold text-white">{{prato.preco}} </div>
+                  <a href="#" class="text-white bg-blue-700   font-medium rounded-lg text-sm px-1 py-1.5 text-center ">Add to cart</a>
               </div>
-            </button>
-        </div>
+            </div>
+          </button>
       </div>
+    </div>
       
-      <!-- Segundo restaurante -->
-      <div class="text-xl font-bold text-white px-28 py-3"> Ratão Burguer </div>
-      <div class=" flex flex-wrap items-center space-x-12 justify-center py-4 bg-cover inset-x-0  " > <!-- Flexbox do cardápio de pratos-->
-        <div v-for="prato in pratos_2" :key="prato.id">
-          <button class=" w-56 h-56 bg-[#541F1B] border-gray-200 rounded-lg shadow transform hover:scale-105 transition duration-300 " @click="addToCart(prato)">
-            <a href="#">
-              <img :src="prato.url" :alt="prato.nome" class= ' object-scale-down h-28 w-full  mx-auto block'>
-            </a>
-            <div class="px-5 pb-5">
-                <a href="#">
-                    <h5 class="text-lg font-semibold tracking-tight text-white ">{{prato.nome}}</h5>
-                </a>
-                <div class="text-xs text-white">{{prato.descricao}} </div>
-                <div class="flex items-center justify-between p-1">
-                    <div class="text-xl font-bold text-white">{{prato.preco}} </div>
-                    <a href="#" class="text-white bg-blue-700   font-medium rounded-lg text-sm px-1 py-1.5 text-center ">Add to cart</a>
-                </div>
+    <!-- Segundo restaurante -->
+    <div class="text-xl font-bold text-white px-28 py-3"> Ratão Burguer </div>
+    <div class="flex flex-wrap items-center space-x-12 justify-center py-4 bg-cover inset-x-0" > <!-- Flexbox do cardápio de pratos-->
+      <div v-for="prato in pratos_2" :key="prato.id">
+        <button class=" w-56 h-56 bg-[#541F1B] border-gray-200 rounded-lg shadow transform hover:scale-105 transition duration-300" @click="addToCart(prato)">
+          <a href="#">
+            <img :src="prato.url" :alt="prato.nome" class= 'object-scale-down h-28 w-full mx-auto block'>
+          </a>
+          <div class="px-5 pb-5">
+              <a href="#">
+                  <h5 class="text-lg font-semibold tracking-tight text-white ">{{prato.nome}}</h5>
+              </a>
+              <div class="text-xs text-white">{{prato.descricao}} </div>
+              <div class="flex items-center justify-between p-1">
+                  <div class="text-xl font-bold text-white">{{prato.preco}} </div>
+                  <a href="#" class="text-white bg-blue-700   font-medium rounded-lg text-sm px-1 py-1.5 text-center ">Add to cart</a>
               </div>
-            </button>
-        </div>
+          </div>
+        </button>
       </div>
-      <div v-if="this.notCompatible">
-              <div class="modal fixed w-full h-full top-0 left-0 flex items-center justify-center">
-                  <div class="modal-overlay absolute w-full h-full bg-gray-900 opacity-50"></div>
-                      <div class="modal-container bg-white w-[300px] mx-auto h-[175px] rounded-[20px] shadow-lg z-50 overflow-y-auto">
-                          <div class="modal-content py-4 text-left px-6">
-                              <div class="modal-body mt-2">
-                                  <!-- Conteúdo do modal aqui -->
-                                  <div class="text-center font-bold">
-                                      <p>Só é possível acrescentar ao carrinho itens de um mesmo restaurante!</p>
-                                  </div>
-                                  <div class="flex justify-center">
-                                    <button @click="fecharModal" class="bg-[#9DBF69] hover:bg-[#A62C21] rounded-lg text-sm px-9 py-2.5 mt-7" >
-                                      Voltar
-                                    </button>
-                                  </div>
-                              </div>
-                          </div>
+    </div>
+
+    <!-- Avisar ao usuário que só pode adicionar itens de um mesmo restaurante no carrinho -->
+    <div v-if="this.notCompatible">
+        <div class="modal fixed w-full h-full top-0 left-0 flex items-center justify-center">
+          <div class="modal-overlay absolute w-full h-full bg-gray-900 opacity-50"></div>
+              <div class="modal-container bg-white w-[300px] mx-auto h-[175px] rounded-[20px] shadow-lg z-50 overflow-y-auto">
+                  <div class="modal-content py-4 text-left px-6">
+                      <div class="modal-body mt-2">
+                          <!-- Conteúdo do modal aqui -->
+                        <div class="text-center font-bold">
+                            <p>Só é possível acrescentar ao carrinho itens de um mesmo restaurante!</p>
+                        </div>
+                        <div class="flex justify-center">
+                        <button @click="fecharModal" class="bg-[#9DBF69] hover:bg-[#A62C21] rounded-lg text-sm px-9 py-2.5 mt-7" >
+                          Voltar
+                        </button>
+                        </div>
                       </div>
                   </div>
-      </div>
-    
-
-    
-
+              </div>
+        </div>
     </div>
-  
-  </template>
+    
+  </div>
+</template>
   
   <script>
   import NavBar from '@/components/NavBar.vue'
